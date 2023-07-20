73783ef97ae8495043603fdabdb32497c2979814
hp <vinayakmalviya21@gmail.com>
2023-07-20 20:37:19 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-
+app.get('/blog',(req,res)=>{
+    res.status(200).json({
+        msg:'blog get request'
+    })
+})
 
 
 module.exports = app;
